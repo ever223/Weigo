e9b346751be6341a1d6a9dbe1a831c104c49c6ab
xiaoo_gan <85265900@qq.com>
2015-11-09 21:51:26 +0800

    初始化项目

---
+++ Weigo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Weigo
+//
+//  Created by xiaoo_gan on 11/9/15.
+//  Copyright © 2015 xiaoo_gan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
